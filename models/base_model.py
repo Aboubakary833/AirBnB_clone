19288e5bed842cf65789380236a779132359a1bc
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-03-23 22:15:05 +0000

    chores: BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Define the Base for model classes."""
+import uuid
+import datetime
+
+time = datetime.datetime
+
+
+class BaseModel:
+    """BaseModel class
+    """
+
+    def __init__(self):
+        """Initialize a new BaseModel
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = time.now()
+        self.updated_at = time.now()
+
+    def __str__(self) -> str:
+        """toString equivalent for BaseModel
+
+        Returns:
+            str: a formatted string of the class
+        """
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += ("({}) {}".format(self.id, self.__dict__))
+        return string
+
+    def save(self):
+        """Updates the public instance attribute updated_at
+        """
+        self.updated_at = time.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+
+        Returns:
+            __dict__: The instance __dict__ representation
+        """
+        __dict = self.__dict__.copy()
+        __dict['__class__'] = self.__class__.__name__
+        __dict["created_at"] = self.created_at.isoformat()
+        __dict["updated_at"] = self.updated_at.isoformat()
+        return __dict
