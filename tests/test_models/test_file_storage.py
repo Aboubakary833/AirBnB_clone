875e8326bbabd62543a73a41d2c6414efe629e04
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-03-26 21:41:38 +0000

    chores: file_storage.py

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
 """test for file storage"""
 import unittest
-import pep8
-import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """FileStorage test class"""
-
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_FileStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_all(self):
         """Tests all"""
