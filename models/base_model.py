b765fc477f020207c59b49211d41a1a857bb08da
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-03-26 21:33:19 +0000

    chores: file_storage.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,14 @@ class BaseModel:
     """BaseModel class
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = time.now()
             self.updated_at = time.now()
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if (key == "id"):
